99b4803b1097c3f4eaac06739712f75b9c6e1592
Lars Bilke <lars.bilke@ufz.de>
2014-12-11 15:07:58 +0100

    Added script for converting VRED viewpoints.
    From, At, Up vectors to Unity Transform.

---
+++ Misc/FromAtUpTransform.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FromAtUpTransform : MonoBehaviour
+{
+	public bool ZUp = false;
+	public Vector3 From;
+	public Vector3 At;
+	public Vector3 Up;
+
+	void OnValidate ()
+	{
+		if (ZUp)
+		{
+			gameObject.transform.position = new Vector3(From.x, From.z, From.y);
+			gameObject.transform.LookAt(new Vector3(At.x, At.z, At.y), new Vector3(Up.x, Up.z, Up.y));
+		}
+		else
+		{
+			gameObject.transform.position = From;
+			gameObject.transform.LookAt(At, Up);
+		}
+	}
+}
