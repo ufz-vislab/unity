ae203bb8e00f54a87aa72ff21af4e8a412e068ba
Lars Bilke <lars.bilke@ufz.de>
2015-01-28 09:41:56 +0100

    Added transition type for viewpoint opacity animation.

--- Misc/ViewpointObjectVisibility.cs
+++ Misc/ViewpointObjectVisibility.cs
@@ -7,10 +7,17 @@ namespace UFZ
 {
 	public class ViewpointObjectVisibility : BaseBehavior<FullSerializerSerializer>
 	{
+		public enum VisibilityTransition
+		{
+			Smooth,
+			StepOnStart,
+			StepOnComplete
+		}
 		public struct ObjectVisibility
 		{
 			public GameObject GameObject;
 			public float Opacity;
+			public VisibilityTransition TransitionType;
 		}
 
 
@@ -25,18 +32,37 @@ namespace UFZ
 				return;
 			}
 
-			viewpoint.OnStart += Execute;
+			viewpoint.OnStart += OnStart;
+			viewpoint.OnFinish += OnComplete;
 
 			foreach (var entry in Entries.Where(entry => entry.GameObject.GetComponent<MaterialProperties>() == null))
 				entry.GameObject.AddComponent<MaterialProperties>();
 		}
 
-		private void Execute(float duration)
+		private void OnStart(float duration)
 		{
 			foreach (var entry in Entries)
 			{
+				if(entry.TransitionType == VisibilityTransition.StepOnComplete)
+					continue;
+
 				var matProps = entry.GameObject.GetComponent<MaterialProperties>();
-				DOTween.To(() => matProps.Opacity, x => matProps.Opacity = x, entry.Opacity, duration);
+				if (entry.TransitionType == VisibilityTransition.Smooth)
+					DOTween.To(() => matProps.Opacity, x => matProps.Opacity = x, entry.Opacity, duration);
+				else
+					matProps.Opacity = entry.Opacity;
+			}
+		}
+
+		private void OnComplete()
+		{
+			foreach (var entry in Entries)
+			{
+				if(entry.TransitionType != VisibilityTransition.StepOnComplete)
+					continue;
+
+				var matProps = entry.GameObject.GetComponent<MaterialProperties>();
+				matProps.Opacity = entry.Opacity;
 			}
 		}
 	}
