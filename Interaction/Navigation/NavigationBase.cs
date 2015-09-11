d01fc9fcbeffffecddf7f5c84d6f3a99a8569ee0
Lars Bilke <lars.bilke@ufz.de>
2015-09-11 11:51:07 +0200

    Fixed another warning.

--- Interaction/Navigation/NavigationBase.cs
+++ Interaction/Navigation/NavigationBase.cs
@@ -85,12 +85,13 @@ namespace UFZ.Interaction
 
 #if UNITY_EDITOR
 			// Disable input when GameView is not focussed
-			var window = EditorWindow.focusedWindow;
 #if UNITY_4_0
+			var window = EditorWindow.focusedWindow;
 			if (window != null && window.title != "UnityEditor.GameView")
 				return;
 #endif
 #if UNITY_5_0
+			var window = EditorWindow.focusedWindow;
 			if (window != null && window.titleContent.text != "Game")
 				return;
 #endif
