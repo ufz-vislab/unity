f0df82dde86b0944716560cbe319ac23d39de088
Lars Bilke <lars.bilke@ufz.de>
2015-11-26 16:09:54 +0100

    Correct blend mode (Fade) for CityEngine billboards (vegetation).

--- Editor/MeshImportSettings.cs
+++ Editor/MeshImportSettings.cs
@@ -38,14 +38,14 @@ namespace UFZ.Initialization
 			//Debug.Log("OnPreprocessModel");
 			var modelImporter = (ModelImporter)assetImporter;
 			modelImporter.animationType = ModelImporterAnimationType.None;
-			modelImporter.optimizeMesh = false;
+			modelImporter.optimizeMesh = IsCityEngine();
 			modelImporter.globalScale = 1.0f;
 			modelImporter.importMaterials = IsCityEngine();
 		}
 
 		private void OnPostprocessModel(GameObject go)
 		{
-			if (!CheckPath(assetPath))
+			if (!CheckPath(assetPath) || IsCityEngine())
 				return;
 
 			//Debug.Log ("OnPostprocessModel for " + go.name);
@@ -161,6 +161,28 @@ namespace UFZ.Initialization
 			AssetDatabase.Refresh();
 		}
 
+		private Material OnAssignMaterialModel(Material material, Renderer renderer)
+		{
+			if (!IsCityEngine()) return null;
+			
+			if (!material.mainTexture || !material.mainTexture.name.Contains("Billboard") ||
+			    (int) material.GetFloat("_Mode") == 2) return null;
+			
+			// Create new materials folder if it not exists
+			if (!AssetDatabase.IsValidFolder((Path.GetDirectoryName(assetPath) + "/Materials")))
+				AssetDatabase.CreateFolder(Path.GetDirectoryName(assetPath), "Materials");
+
+			// Workaround: sometimes materials have weird names such as CreateFan.. use mainTexture as name
+			var materialPath = Path.GetDirectoryName(assetPath) + "/Materials/" + material.mainTexture.name + ".mat";
+			if (AssetDatabase.LoadAssetAtPath(materialPath, typeof (Material)))
+				return AssetDatabase.LoadAssetAtPath(materialPath, typeof (Material)) as Material;
+
+			SetupStandardMaterialWithBlendMode(material, BlendMode.Fade);
+
+			AssetDatabase.CreateAsset(material, materialPath);
+			return material;
+		}
+
 		private void OnPreprocessTexture()
 		{
 			if (!CheckPath(assetPath))
@@ -233,6 +255,62 @@ namespace UFZ.Initialization
 
 			mesh.SetIndices(indicesForLines.ToArray(), MeshTopology.Lines, 0);
 			mesh.RecalculateBounds();
+		}
+
+		public enum BlendMode
+		{
+			Opaque,
+			Cutout,
+			Fade,		// Old school alpha-blending mode, fresnel does not affect amount of transparency
+			Transparent // Physically plausible transparency mode, implemented as alpha pre-multiply
+		}
+
+		// From StandardShaderGUI.cs in Unity builtin Shader package, https://unity3d.com/get-unity/download/archive
+		public static void SetupStandardMaterialWithBlendMode(Material material, BlendMode blendMode)
+		{
+			switch (blendMode)
+			{
+				case BlendMode.Opaque:
+					material.SetOverrideTag("RenderType", "");
+					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+					material.SetInt("_ZWrite", 1);
+					material.DisableKeyword("_ALPHATEST_ON");
+					material.DisableKeyword("_ALPHABLEND_ON");
+					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+					material.renderQueue = -1;
+					break;
+				case BlendMode.Cutout:
+					material.SetOverrideTag("RenderType", "TransparentCutout");
+					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+					material.SetInt("_ZWrite", 1);
+					material.EnableKeyword("_ALPHATEST_ON");
+					material.DisableKeyword("_ALPHABLEND_ON");
+					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+					material.renderQueue = 2450;
+					break;
+				case BlendMode.Fade:
+					material.SetOverrideTag("RenderType", "Transparent");
+					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+					material.SetInt("_ZWrite", 0);
+					material.DisableKeyword("_ALPHATEST_ON");
+					material.EnableKeyword("_ALPHABLEND_ON");
+					material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+					material.renderQueue = 3000;
+					break;
+				case BlendMode.Transparent:
+					material.SetOverrideTag("RenderType", "Transparent");
+					material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+					material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+					material.SetInt("_ZWrite", 0);
+					material.DisableKeyword("_ALPHATEST_ON");
+					material.DisableKeyword("_ALPHABLEND_ON");
+					material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+					material.renderQueue = 3000;
+					break;
+			}
 		}
 	}
 }
