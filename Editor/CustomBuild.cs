c9cb3fd79b720b99374541a8616526fabafa1614
Lars Bilke <lars.bilke@ufz.de>
2016-02-16 14:16:23 +0100

    Added custom build script for Advanced Builder.

---
+++ Editor/CustomBuild.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using PygmyMonkey.AdvancedBuilder;
+
+public class CustomBuild : IAdvancedCustomBuild
+{
+	public void OnPreBuild(Configuration configuration, DateTime buildDate)
+	{
+		// Do nothing
+	}
+
+	public void OnPostBuild(Configuration configuration, DateTime buildDate)
+	{
+		var process = new Process
+		{
+			StartInfo =
+			{
+				FileName = @"C:\tools\cwRsync_5.5.0_x86_Free\cwrsync.cmd"
+			}
+		};
+		process.Start();
+	}
+}
