ca67f693ed8dfff2ec5d9034be1c26f13afa95fc
Lars Bilke <lars.bilke@ufz.de>
2016-04-20 12:00:09 +0200

    WebView works.

--- UI/Views/WebView.cs
+++ UI/Views/WebView.cs
@@ -5,12 +5,6 @@ using MarkUX.Views;
 namespace MarkUX.Views
 {
 	[InternalView]
-	[RemoveComponent(typeof(UnityEngine.UI.Image))]
-	[RemoveComponent(typeof(RectTransform))]
-	//[AddComponent(typeof(MeshFilter))]
-	//[AddComponent(typeof(MeshRenderer))]
-	//[AddComponent(typeof(VRWebView))]
-	//[AddComponent(typeof(VRActor))]
 	public class WebView : View
 	{
 		#region Fields
@@ -18,7 +12,7 @@ namespace MarkUX.Views
 		[ChangeHandler("UpdateURL")]
 		public string URL;
 		private bool _initialized;
-		private VRWebView vrWebView;
+		private VRWebView _vrWebView;
 
 		#endregion
 
@@ -27,6 +21,11 @@ namespace MarkUX.Views
 		public WebView()
 		{
 			URL = "http://www.ufz.de/index.php?en=37716";
+
+			Width = new ElementSize(1f, ElementSizeUnit.Percents);
+			
+			// TODO: Hardcoded height
+			Height = new ElementSize(520f, ElementSizeUnit.Pixels);
 		}
 
 		#endregion
@@ -37,25 +36,32 @@ namespace MarkUX.Views
 		{
 			base.Initialize();
 			this.ForEachChild<View>(x => x.Deactivate(), false);
-			//_parent = Parent != null ? Parent.GetComponent<View>() : null;
 
-			// TODO: Create once!
+			if(!Application.isPlaying)
+				return;
+
+			var height = (int) ActualHeight;
+			var width = (int) ActualWidth;
+
 			var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-			vrWebView = plane.AddComponent<VRWebView>();
-			plane.AddComponent<VRActor>();
+			plane.transform.position = new Vector3(0.5f, -0.005f, 0.998f);
+			plane.transform.rotation = Quaternion.Euler(90, 180, 0);
+			plane.transform.localScale = new Vector3(width * 0.0001f, 1f, height * 0.0001f);
 			plane.transform.parent = transform;
-			//var meshFilterComponent = GetComponent<MeshFilter>();
-			//meshFilterComponent.sharedMesh = plane.GetComponent<MeshFilter>().mesh;
-			//var meshRenderer = GetComponent<MeshRenderer>();
-			//meshRenderer.material = new Material(Shader.Find("Diffuse"));
-			//Destroy(plane);
+			plane.GetComponent<MeshCollider>().convex = true;
+			plane.AddComponent<VRActor>();
+
+			_vrWebView = plane.AddComponent<VRWebView>();
+			_vrWebView.m_Width = width;
+			_vrWebView.m_Height = height;
+
 			UpdateURL();
 		}
 
 		public virtual void UpdateURL()
 		{
-			//var webViewComponent = GetComponent<VRWebView>();
-			vrWebView.webView.SetURL(URL);
+			if(_vrWebView != null && _vrWebView.webView != null)
+				_vrWebView.webView.SetURL(URL);
 		}
 
 		#endregion
