a69c3bf0726cd1e0004a56a0c1fe2ebbadef8bf1
Lars Bilke <lars.bilke@ufz.de>
2016-05-13 16:35:11 +0200

    Added InjectionManager.

---
+++ Views/InjectionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using MarkLight;
+using ModestTree;
+using UFZ.Initialization;
+using UFZ.IOC;
+using Zenject;
+
+public class InjectionManager : View
+{
+	[ChangeHandler("InstallerChanged")]
+	public UfzInstaller UfzInstaller;
+	public SceneCompositionRoot SceneCompositionRoot;
+	public Core Core;
+
+	protected virtual void InstallerChanged()
+	{
+		SceneCompositionRoot.OnlyInjectWhenActive = true;
+		SceneCompositionRoot.Installers = new MonoInstaller[1];
+		SceneCompositionRoot.Installers[0] = UfzInstaller;
+	}
+}
