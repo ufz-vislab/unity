65eb6c4487617150074d7ed905d866c5fe6c93db
Lars Bilke <lars.bilke@ufz.de>
2016-05-18 11:40:10 +0200

    Added UfzEventSystem enabling Wand inputs.

---
+++ Views/UfzEventSystem.cs
@@ -0,0 +1,23 @@
+﻿using MarkLight;
+using UFZ.Initialization;
+using UnityEngine;
+
+[ExcludeComponent("UnityEventSystem")]
+public class UfzEventSystem : MarkLight.Views.EventSystem
+{
+	public WandInputModule WandInputModule;
+
+	public void Awake()
+	{
+		var cursor = SRResources.WandCursor.Instantiate();
+		cursor.transform.SetParent(transform, false);
+		WandInputModule.cursor = cursor.GetComponent<RectTransform>();
+
+		var globalInits = FindObjectOfType<GlobalInits>();
+		if (globalInits.GuiInputType == GlobalInits.InputType.Mouse)
+		{
+			WandInputModule.enabled = false;
+			WandInputModule.cursor.gameObject.SetActive(false);
+		}
+	}
+}
