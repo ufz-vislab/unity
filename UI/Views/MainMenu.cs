e16d7fabc76f17f0b134c43ab023ced50f4e9291
Lars Bilke <lars.bilke@ufz.de>
2016-06-17 15:41:47 +0200

    MainMenu uses DragableUIView.

--- UI/Views/MainMenu.cs
+++ UI/Views/MainMenu.cs
@@ -1,14 +1,16 @@
-using UnityEngine;
 using MarkLight.Views.UI;
-using MiddleVR_Unity3D;
 
 public class MainMenu : UIView
 {
-	public string Caption = "Main Menu";
 	public ViewSwitcher ContentViewSwitcher;
+	public DragableUIView DragableUIView;
 
-	private bool dragging = false;
-	private Vector3 lastPointerPos;
+	public override void Initialize()
+	{
+		base.Initialize();
+
+		DragableUIView.SetValue("Caption", "Main Menu");
+	}
 
 	public void SectionSelected(ItemSelectionActionData eventData)
 	{
@@ -20,35 +22,4 @@ public class MainMenu : UIView
 		Deactivate();
 	}
 
-	public void DragStart()
-	{
-		dragging = true;
-		if (UFZ.IOC.Core.Instance.Environment.HasDevice("Flystick"))
-			lastPointerPos = transform.InverseTransformPoint(FindObjectOfType<WandInputModule>().cursor.position);
-		else
-			lastPointerPos = UFZ.IOC.Core.Instance.Mouse.Position();
-	}
-
-	public void DragEnd()
-	{
-		dragging = false;
-	}
-
-	public void Update()
-	{
-		if (!dragging)
-			return;
-
-		Vector3 currentPointerPos;
-		if (UFZ.IOC.Core.Instance.Environment.HasDevice("Flystick"))
-			currentPointerPos = transform.InverseTransformPoint(FindObjectOfType<WandInputModule>().cursor.position);
-		else
-			currentPointerPos = UFZ.IOC.Core.Instance.Mouse.Position();
-		var deltaPointerPos = currentPointerPos - lastPointerPos;
-		transform.localPosition += deltaPointerPos;
-		lastPointerPos = currentPointerPos;
-		// Take movement of UI into account for last pointer position
-		if (UFZ.IOC.Core.Instance.Environment.HasDevice("Flystick"))
-			lastPointerPos -= deltaPointerPos;
-	}
 }
