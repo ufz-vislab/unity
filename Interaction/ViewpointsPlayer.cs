a305d9570646437cb261757883dcfcb783f68404
Lars Bilke <lars.bilke@ufz.de>
2016-10-14 13:51:50 +0200

    Added ViewpointsPlayer.

---
+++ Interaction/ViewpointsPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FullInspector;
+using UnityEngine;
+
+namespace UFZ.Interaction
+{
+	public class ViewpointsPlayer : BaseBehavior
+	{
+		//[SerializeField]
+		public struct ViewpointData
+		{
+			public Viewpoint Viewpoint;
+			public bool Override;
+			public float Speed;
+			public float MaxTransitionTime;
+		}
+		public bool PlayAtStart;
+		public bool Loop = true;
+		public float WaitTime = 5;
+		public List<ViewpointData> ViewpointsData;
+
+		private int _currentViewpoint;
+
+		void Start()
+		{
+			if (PlayAtStart)
+				ViewpointsData[0].Viewpoint.Move();
+
+			foreach (var viewpoint in ViewpointsData)
+			{
+				if (viewpoint.Override)
+				{
+					viewpoint.Viewpoint.MaxTransitionTime = viewpoint.MaxTransitionTime;
+					viewpoint.Viewpoint.Speed = viewpoint.Speed;
+				}
+				viewpoint.Viewpoint.OnFinish += delegate
+				{
+					if (_currentViewpoint == ViewpointsData.Count - 1)
+					{
+						if (Loop)
+							_currentViewpoint = 0;
+						else
+							return;
+					}
+					else
+						_currentViewpoint++;
+					
+					StartCoroutine(Move(ViewpointsData[_currentViewpoint].Viewpoint));
+				};
+			}
+		}
+
+		private IEnumerator Move(Viewpoint viewpoint)
+		{
+			yield return new WaitForSeconds(WaitTime);
+			viewpoint.Move();
+		}
+
+		[InspectorButton]
+		public void Play()
+		{
+			ViewpointsData[_currentViewpoint].Viewpoint.Move();
+		}
+	}
+}
