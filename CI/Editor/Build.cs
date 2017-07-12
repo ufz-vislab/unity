2d7817a66bf565f68c97109a53150de328f3970e
Lars Bilke <lars.bilke@ufz.de>
2017-07-12 10:58:45 +0200

    Added CI build script.
    Usage:
    
    "C:\Program Files\Unity-5.6\Editor\Unity.exe" -quit -batchMode -logFile D:\projects\all\logs.txt -projectPath D:\projects\all -executeMethod UFZ.CI.Build.RunSingleBuild

---
+++ CI/Editor/Build.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.UI.WebControls;
+using PygmyMonkey.AdvancedBuilder;
+
+namespace UFZ.CI
+{
+	public class Build
+	{
+		[UnityEditor.MenuItem("UFZ/CI/Build active")]
+		public static void RunSingleBuild()
+		{
+			var builder = AdvancedBuilder.Get();
+			var index = builder.getProjectConfigurations().selectedConfigurationIndex;
+			var config = builder.getProjectConfigurations().configurationList[index];
+			AdvancedBuilder.PerformBuild(config);
+		}
+
+		[UnityEditor.MenuItem("UFZ/CI/Build all")]
+		public static void RunAllBuilds()
+		{
+			AdvancedBuilder.PerformBuild();
+		}
+	}
+}
