9bd0f0b41c7031ca1597e8215e790407e9a251aa
Lars Bilke <lars.bilke@ufz.de>
2017-07-21 12:46:22 +0200

    Added Menu tool for showing hidden objects in hierarchy.

---
+++ Editor/MenuTools.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UFZ.Editor
+{
+	public class MenuTools
+	{
+		[MenuItem("UFZ/Tools/Unhide All")]
+		static void ShowAll()
+		{
+			var allObjects = GameObject.FindObjectsOfType<GameObject>();
+			foreach (var g in allObjects)
+			{
+				g.hideFlags = g.hideFlags & ~HideFlags.HideInHierarchy;
+			}
+		}
+	}
+}
