3f4dd40dba0add96ad163f25af4c69313dacddc5
Lars Bilke <lars.bilke@ufz.de>
2017-08-30 15:07:05 +0200

    Added CityEngineMaterialProperties implementing IHideable.

---
+++ Rendering/CityEngineMaterialProperties.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace UFZ.Rendering
+{
+	public class CityEngineMaterialProperties : SerializedMonoBehaviour, IHideable
+	{
+
+		[ShowInInspector]
+		public bool Enabled
+		{
+			get { return _enabled; }
+			set
+			{
+				_enabled = value;
+				foreach (var meshRenderer in gameObject.GetComponentsInChildren<MeshRenderer>())
+				{
+					meshRenderer.enabled = value;
+				}
+			}
+		}
+		[SerializeField, HideInInspector]
+		private bool _enabled = true;
+	}
+}
